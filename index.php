416b2fe31c5b9bef746174f87613816ca0701b5e
Wilsonmobisa <willexjumah12338@gmail.com>
2023-08-21 22:05:30 +0300

    Collect Data

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+    <form method="post" action="Data.php">
+        Name: <input type="text" name="fname" id="">
+        Phone Number: <input type="tel" name="tel" id="">
+        Country: <select name="country" id="">
+        <option value="" style="display: none;"></option>
+            <option value="Kenya">Kenya</option>
+            <option value="Uganda">Uganda</option>
+            <option value="Tanzania">Tanzania</option>
+            <option value="Rwanda">Rwanda</option>
+        </select>
+    </form>
+</html>
